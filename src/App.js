6c87f04e5d1abc304bcd891dfa14281c6389160b
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-01-06 11:32:55 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home';
+import SignUp from './Pages/SignUp';
+import Offers from './Pages/Offers';
+import ForgotPassword from './Pages/ForgotPassword';
+import Profile from './Pages/Profile';
+import SignIn from './Pages/SignIn';
 
 function App() {
   return (
-    <div className="">
-     <h1 className='text-2xl bg-red-400'>Hello world</h1>
-    </div>
+    <>
+      <Router>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/profile' element={<Profile/>}/>
+                <Route path='/sign-in' element={<SignIn/>}/>
+                <Route path='/sign-up' element={<SignUp/>}/>
+                <Route path='/forgot-password' element={<ForgotPassword/>}/>
+                <Route path='/offers' element={<Offers/>}/>
+            </Routes>
+        </Router>
+   
+    </>
   );
 }
 
